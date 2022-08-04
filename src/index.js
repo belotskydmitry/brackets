373a8a9294c956ed9ca6c54f26812b43ab8dea4d
DmitryBelotsky <belotsky.dmitry@gmail.com>
2022-08-04 13:19:25 +0300

    init: create task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let st = [];
+  let br = bracketsConfig.join('').replace(/,/g, '');
+
+  for (let bracket of str) {
+    let brIn = br.indexOf(bracket);
+
+    if(brIn % 2 === 0) {
+      st.push(brIn);
+
+      if (bracket === br[brIn + 1]) {
+        st.pop();
+      }
+    } else {
+      if (st.pop() !== brIn - 1) {
+        return false;
+      }
+    }
+  } return st.length === 0;
 }
